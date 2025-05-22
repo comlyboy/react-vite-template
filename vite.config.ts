7041189af1609300d93f580a03febd6984d8c4ca
Cornelius Okeke <okekecornelius@gmail.com>
2025-05-22 12:01:03 +0100

    improved vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	server: {
+		port: 3000,
+		strictPort: true,
+	},
 });
-
-// https://vitejs.dev/config/
-// export default defineConfig((config) => {
-// 	const env = loadEnv(config.mode, process.cwd(), '');
-// 	return {
-// 		define: {
-// 			'process.env': env
-// 		},
-// 		plugins: [react()],
-// 		resolve: {
-// 			alias: {
-// 				"@corneliusokeke": path.resolve(__dirname, "./src"),
-// 			},
-// 		},
-// 		// build: {
-// 		// 	rollupOptions: {
-// 		// 		output: {
-// 		// 			manualChunks: undefined,
-
-// 		// 			entryFileNames: `assets/gravity_[hash]_[name]_[hash].js`,
-// 		// 			chunkFileNames: `assets/gravity_[hash]_[name]_[hash].js`,
-// 		// 			assetFileNames: `assets/gravity_[hash]_[name]_[hash].[ext]`
-// 		// 		}
-// 		// 	},
-// 		// 	cssCodeSplit: false,
-// 		// }
-// 	}
-
-// });
